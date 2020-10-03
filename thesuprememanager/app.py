25be3db34aeba9dae83dfe29c140d56cd525d7a7
David Frankel <42774874+frankeld@users.noreply.github.com>
2020-10-03 18:45:26 -0500

    Add year updater

---
+++ thesuprememanager/app.py
@@ -0,0 +1,90 @@
+import time
+from flask import Flask, render_template, request
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from selenium.webdriver.firefox.options import Options
+import os
+
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__)
+
+    cred = credentials.Certificate("../firebase-key.json")
+    firebase_admin.initialize_app(cred, {
+    'projectId': 'thesupremehacks',
+    })
+    db = firestore.client()
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/year', methods=['GET'])
+    def year():
+        return render_template('year.html')
+
+    @app.route('/<string:docket>')
+    def update_docket(docket):
+        return 'test'
+
+    @app.route('/api/year/<string:year>')
+    def update_year(year):
+        if 2+3 == 5:
+            time.sleep(3)
+            return 'done'
+        def repair(string):
+            string = BeautifulSoup(string, 'html.parser').text
+            string = string.replace(u'\xa0', u' ')
+            return string
+
+        # show the post with the given id, the id is an integer
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        YEAR = year
+        try:
+            driver.get('https://www.oyez.org/cases/' + YEAR)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href^="cases/'+ YEAR +'/"]'))
+            )
+
+            DATE_TYPES = ['granted', 'argued', 'decided', 'citation']
+
+            elements = driver.find_elements(By.CSS_SELECTOR, 'ul.index.ng-scope li.ng-scope')
+            print('here')
+            cases = []
+            for ele in elements:
+                info = {}
+                title = ele.find_element(By.TAG_NAME, 'h2')
+                link = ele.find_element(By.CSS_SELECTOR, 'a[href^="cases/'+ YEAR +'/"]')
+                id = link.get_attribute('href').split('/')[-1]
+                description = ele.find_element(By.CLASS_NAME, 'description')
+                dates = []
+                for date in DATE_TYPES:
+                    val = ele.find_element(By.CLASS_NAME, date).find_elements(By.TAG_NAME, 'div')[0]
+                    if val == '':
+                        val = ele.find_element(By.CLASS_NAME, date).find_elements(By.TAG_NAME, 'div')[-1]
+                    dates.append(val.text)
+                info.update({'title': title.text, 'id': id, 'description': description.text, 'dates': dates})
+                response = requests.get('https://api.oyez.org/cases/'+YEAR+'/'+id).json()
+                info.update({'petitioner': response['first_party'], 'respondent': response['second_party'], 'lower_court': response['lower_court']['name'], 'facts': repair(response['facts_of_the_case']), 'question': repair(response['question'])})
+                cases.append(info)
+                
+            print('here3')
+            cases_collection = db.collection('cases')
+            print('here2')
+            for case in cases:
+                cases_collection.document(case['id']).set(case, merge=True)
+        finally:
+            driver.quit()
+        return 'done'
+    return app
